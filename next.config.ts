41953a02dff908f4f76991a7925fbc55f8d6bae6
rovilay <rovilayjnr01@gmail.com>
2025-03-01 15:51:55 -0500

    add api

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    PROVIDER_URL: process.env.PROVIDER_URL
+  }
 };
 
 export default nextConfig;
